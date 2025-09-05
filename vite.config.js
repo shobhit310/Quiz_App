9637ef0878e9cdc5d6e345b93f1f45b1ae750e40
finemen <shobhitdwivedi667@gmail.com>
2025-09-05 09:43:09 +0530

    Updated quiz app with latest changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",   // ✅ works on Render & most static hosts
+  base: "",   // ✅ empty base for root deployment like Render
 })
+
