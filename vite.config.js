b3c7502de8ec3e5982692941572fd39bc4a51a06
finemen <shobhitdwivedi667@gmail.com>
2025-09-05 09:39:47 +0530

    Updated quiz app with latest changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Quiz_App/",   // 👈 this line tells Vite the repo name for GitHub Pages
+  base: "./",   // ✅ works on Render & most static hosts
 })
